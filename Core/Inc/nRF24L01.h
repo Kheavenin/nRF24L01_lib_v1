b938ee2f5f67b46ff0639485fbbaf44e0613a478
Deuter <wojciechbernat95@gmail.com>
2020-11-01 09:38:48 +0100

    Added files for new library version

---
+++ Core/Inc/nRF24L01.h
@@ -0,0 +1,13 @@
+/*
+ * nRF24L01.h
+ *
+ *  Created on: 1 lis 2020
+ *      Author: Khevenin
+ */
+
+#ifndef INC_NRF24L01_H_
+#define INC_NRF24L01_H_
+
+
+
+#endif /* INC_NRF24L01_H_ */
---
+++ Core/Src/nRF242L01.c
@@ -0,0 +1,8 @@
+/*
+ * nRF242L01.c
+ *
+ *  Created on: 1 lis 2020
+ *      Author: Khevenin
+ */
+
+
