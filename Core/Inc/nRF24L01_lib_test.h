87f4004ef458f19774592c7d23ad6552269da4dc
Deuter <wojciechbernat95@gmail.com>
2020-11-11 22:01:56 +0100

    Added file for tests functions and cases.

---
+++ Core/Inc/nRF24L01_lib_test.h
@@ -0,0 +1,13 @@
+/*
+ * nRF24L01_lib_test.h
+ *
+ *  Created on: 11 lis 2020
+ *      Author: Khevenin
+ */
+
+#ifndef INC_NRF24L01_LIB_TEST_H_
+#define INC_NRF24L01_LIB_TEST_H_
+
+
+
+#endif /* INC_NRF24L01_LIB_TEST_H_ */
---
+++ Core/Src/nRF24L01_lib_test.c
@@ -0,0 +1,8 @@
+/*
+ * nRF24L01_lib_test.c
+ *
+ *  Created on: 11 lis 2020
+ *      Author: Khevenin
+ */
+
+
